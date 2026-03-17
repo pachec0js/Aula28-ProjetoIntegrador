67fdac82f95354d46b7655da962e2b27484b2367
pachec0js <fabio.h.l.pacheco@gmail.com>
2026-03-17 14:58:19 -0300

    feat: adição base ficcional de dados dos alunos

---
+++ dados.py
@@ -0,0 +1,10 @@
+alunos = [
+    ("Arthur", [10, 8, 9]),
+    ("Bruno", [7, 8, 9]),
+    ("Fábio", [10, 8, 9]),
+    ("Felipe", ['erro', 8, 9]),
+    ("Guilherme", [10, 8, 9]),
+    ("Pedro", [10, 'falha', 9]),
+    ("Renato", [None, 8, 9]),
+    ("André", [])
+]
